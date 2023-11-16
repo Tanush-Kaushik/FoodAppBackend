c4ed4a8e05e09b7c36d79dfb6f3637e1dc252b80
Tanush-Kaushik <141478739+Tanush-Kaushik@users.noreply.github.com>
2023-11-16 12:43:25 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -26,35 +26,35 @@ export const user = mongoose.model('user', schema);
 
 
 
-const food_itemsSchema = new mongoose.Schema({
-    CategoryName: String,
-    name: String,
-    img: String,
-    description: String,
-    options: {
-        half: Number,
-        full: Number
-    }
-})
-export const food_item = mongoose.model('food_item',food_itemsSchema)
+// const food_itemsSchema = new mongoose.Schema({
+//     CategoryName: String,
+//     name: String,
+//     img: String,
+//     description: String,
+//     options: {
+//         half: Number,
+//         full: Number
+//     }
+// })
+// export const food_item = mongoose.model('food_item',food_itemsSchema)
 
  
 
-const foodCategorySchema = new mongoose.Schema({
-    CategoryName:String
-})
-export const food_category = mongoose.model('food_category',foodCategorySchema)
+// const foodCategorySchema = new mongoose.Schema({
+//     CategoryName:String
+// })
+// export const food_category = mongoose.model('food_category',foodCategorySchema)
 
 
-const orderSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    order_data:{
-        type:Array,
-        required:true
-    }
-})
-export const orders = mongoose.model('orders',orderSchema)
+// const orderSchema = new mongoose.Schema({
+//     email:{
+//         type:String,
+//         required:true,
+//         unique:true
+//     },
+//     order_data:{
+//         type:Array,
+//         required:true
+//     }
+// })
+// export const orders = mongoose.model('orders',orderSchema)
