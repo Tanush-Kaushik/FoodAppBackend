e958d30c65191bd8181b2a844e9e9f442e83b702
Tanush-Kaushik <141478739+Tanush-Kaushik@users.noreply.github.com>
2024-01-16 19:34:34 +0530

    Update OrderData.js

--- routes/OrderData.js
+++ routes/OrderData.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { orders } from "../models/order.js";
+import nodemailer from 'nodemailer';
 
 export const router3 = express.Router()
 
@@ -46,6 +47,35 @@ router3.post('/orderData', async (req, res) => {
             res.send('Server error in updating user data')
         }
     }
+
+
+    //
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS
+        }
+    });
+  
+    var mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: 'FOODY',
+        text: 'FOODY' , 
+        html: '<h3>Thanks for ordering from our service. Be sure to check out other tasty options!</h3></br><h4>https://foodappfrontend.onrender.com/</h4>'
+    };
+  
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
+    //
 
 })
 
